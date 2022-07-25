54e12689b2944e9e2e63f296117ddcfecb7ed1e0
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-07-24 21:28:12 -0700

    Initial commit

---
+++ src/main/java/com/technovision/alchemistry/Alchemistry.java
@@ -0,0 +1,23 @@
+package com.technovision.alchemistry;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Chemistry mod that allows you to break down items
+ * into elements and recombine them into different items.
+ *
+ * @version 1.0.0
+ * @author TechnoVision
+ */
+public class Alchemistry implements ModInitializer {
+
+    public static String MOD_ID = "alchemistry";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
