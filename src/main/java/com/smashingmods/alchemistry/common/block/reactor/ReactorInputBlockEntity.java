2c5423beed71506ffd438265dc17155c755f1ecd
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-08-01 23:13:40 -0700

    Add item pipe compatibility to reactor input block

--- src/main/java/com/smashingmods/alchemistry/common/block/reactor/ReactorInputBlockEntity.java
+++ src/main/java/com/smashingmods/alchemistry/common/block/reactor/ReactorInputBlockEntity.java
@@ -1,17 +1,22 @@
 package com.smashingmods.alchemistry.common.block.reactor;
 
 import com.smashingmods.alchemistry.api.blockentity.AbstractReactorBlockEntity;
+import com.smashingmods.alchemistry.api.blockentity.ImplementedInventory;
 import com.smashingmods.alchemistry.registry.BlockEntityRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 import javax.annotation.Nullable;
 
-public class ReactorInputBlockEntity extends BlockEntity {
+public class ReactorInputBlockEntity extends BlockEntity implements ImplementedInventory {
 
     @Nullable
     private AbstractReactorBlockEntity controller;
+    private DefaultedList<ItemStack> tempInv = DefaultedList.ofSize(1, ItemStack.EMPTY);
 
     public ReactorInputBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.REACTOR_INPUT_BLOCK_ENTITY, pos, state);
@@ -24,12 +29,20 @@ public class ReactorInputBlockEntity extends BlockEntity {
 
     public void setController(@Nullable AbstractReactorBlockEntity controller) {
         this.controller = controller;
-        //noinspection ConstantConditions
-        //this.lazyInputHandler = LazyOptional.of(() -> controller.getInputHandler());
     }
 
-    public void onControllerRemoved() {
-        controller = null;
-        //lazyInputHandler = LazyOptional.empty();
+    @Override
+    public boolean canInsert(int slot, ItemStack stack, @org.jetbrains.annotations.Nullable Direction side) {
+        return true;
+    }
+
+    @Override
+    public boolean canExtract(int slot, ItemStack stack, Direction side) {
+        return false;
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return (controller != null) ? controller.getItems() : tempInv;
     }
 }
